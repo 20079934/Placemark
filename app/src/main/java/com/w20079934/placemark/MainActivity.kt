7184991b7d3b5db41a6bea75a78af1dcb861a1e3
20079934 <20079934@mail.wit.ie>
2020-12-23 15:15:24 +0000

    Initial Commit

---
+++ app/src/main/java/com/w20079934/placemark/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.w20079934.placemark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    //test
+}
